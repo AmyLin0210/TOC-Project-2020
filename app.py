c602c87a8916212bb9c3d8ab30c991d1e776f612
AmyLin <amy2791530@gmail.com>
2019-11-24 23:19:19 +0800

    change port

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ if __name__ == "__main__":
     # port = os.environ.get("PORT", 8000)
     # app.run(host="0.0.0.1", port=port, debug=True)
     port = os.environ['PORT']
-    app.run(host="0.0.0.0", port=PORT, debug=True, reloader=True)
+    app.run(host="0.0.0.0", port=port, debug=True, reloader=True)
